5313c40e7baf635b3fa9892b98c3e10b6c38833f
YannRosemaRWS <yann.rosema@rws.nl>
2020-09-11 13:05:04 +0200

    async berichten lezen

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-# import requests
+import requests
 import asyncio
 import websockets
 import logging
 import json
 logger = logging.getLogger('websockets')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 
-# r = requests.get('http://10.0.0.26/api/device/info')
-
-async def hello():
+async def receiveMessage(closing,closed):
     uri = "ws://10.0.0.26:13314/api/subscriptions"
     async with websockets.connect(uri) as websocket:
         request = json.dumps({"messageType":"Subscription","subscription":{"type":"Event","action":"Subscribe"}})
@@ -19,10 +17,42 @@ async def hello():
         response = await websocket.recv()
         jsonResponse = json.loads(response)
         if jsonResponse["subscription"]["returnValue"] == "OK":
-            while True:
-                response = await websocket.recv()
-                jsonResponse = json.loads(response)
-                print(f"{jsonResponse}")
+            while not closing.is_set():
+                try:
+                    response = await asyncio.wait_for(websocket.recv(), timeout=1.0)
+                    jsonResponse = json.loads(response)
+                    logger.info(f"{jsonResponse}")
+                except asyncio.TimeoutError as e:
+                    logger.debug(f"Timeout!")
 
-asyncio.get_event_loop().run_until_complete(hello())
-# asyncio.get_event_loop().run_forever()
+    closed.set()
+
+async def main():
+    r = requests.get('http://10.0.0.26:13314/api/device/info')
+    logger.debug(f"{r.text}")
+
+    closing = asyncio.Event()
+    closed = asyncio.Event()
+
+    logger.debug(f"Starting IR receiver task")
+    subscriptionTask = asyncio.create_task(receiveMessage(closing,closed))
+
+    sleepTime = 10.0
+    logger.debug(f"Sleeping for {sleepTime}s, zzz...")
+    await asyncio.sleep(sleepTime)
+
+    logger.debug(f"Closing..")
+    closing.set()
+    await closed.wait()
+
+    logger.debug(f"Everything closed!")
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    except Exception as e:
+        pass
+    finally:
+        loop.close()
+
