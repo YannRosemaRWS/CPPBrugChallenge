7c6933993355126c82bb8bd17b15c71486b70e54
YannRosemaRWS <yann.rosema@rws.nl>
2020-09-22 09:14:35 +0200

    IR config laden en objecten maken

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import xml.etree.ElementTree as ET
 import xmlschema
 import pprint
 import uuid as UUID
+import cameratransform as ct
+import numpy as np
 
 class DataParseError(Exception):
     """Exception raised for XML data parsing errors.
@@ -16,34 +18,11 @@ class DataParseError(Exception):
         self.message = message
         super().__init__('XML element ' +  element + ' raised: ' + self.message)
 
-class Drawable:
-    def __init__(self, length:float, width:float, x:float, y:float):
-        self.length = length
-        self.width = width
+class Point(object):
+    def __init__(self, x:float, y:float):
+        super().__init__()
         self.x = x
         self.y = y
-
-    @property
-    def length(self):
-        return self._length
-
-    @length.setter
-    def length(self, length):
-        if length <= 0:
-            raise ValueError('Length cannot be smaller or equal to 0.')
-        else:
-            self._length = length
-
-    @property
-    def width(self):
-        return self._width
-
-    @width.setter
-    def width(self, width):
-        if width <= 0:
-            raise ValueError('Width cannot be smaller or equal to 0.')
-        else:
-            self._width = width
 
     @property
     def x(self):
@@ -64,7 +43,53 @@ class Drawable:
     def draw(self):
         pass
 
-class Brug(Drawable):
+class Polygon(object):
+    def __init__(self, points:list):
+        self.points = points
+
+    @property
+    def points(self):
+        return self._points
+
+    @points.setter
+    def points(self, points):
+        self._points = points
+
+    def draw(self):
+        pass
+
+class Rectangle(Polygon):
+    def __init__(self, length:float, width:float, x:float, y:float):
+        self.length = length
+        self.width = width
+        self.x = x
+        self.y = y
+        points = [(x, y), (x + width, y), (x, y + length), (x + width, y + length)]
+        super().__init__(points)
+
+    @property
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self, length):
+        if length < 0:
+            raise ValueError('Length cannot be smaller than 0.')
+        else:
+            self._length = length
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        if width < 0:
+            raise ValueError('Width cannot be smaller than 0.')
+        else:
+            self._width = width
+
+class Brug(Rectangle):
     def __init__(self, length:float, width:float, uuid:UUID.UUID, name:str = None):
         super().__init__(length, width, 0.0, 0.0)
         self.uuid = uuid
@@ -90,7 +115,7 @@ class Brug(Drawable):
     def addRijbaan(self, rijbaan):
         self.rijbanen.append(rijbaan)
 
-class Rijbaan(Drawable):
+class Rijbaan(Rectangle):
     def __init__(self, brug:Brug, width:float, x:float, y:float, uuid:UUID.UUID):
         super().__init__(brug.length, width, x, y)
         self.uuid = uuid
@@ -107,7 +132,7 @@ class Rijbaan(Drawable):
     def addRijstrook(self, rijstrook):
         self.rijstroken.append(rijstrook)
 
-class Rijstrook(Drawable):
+class Rijstrook(Rectangle):
     def __init__(self, rijbaan:Rijbaan, width:float, x:float, y:float, uuid:UUID.UUID, trafficType:str):
         super().__init__(rijbaan.length, width, x, y)
         self.uuid = uuid
@@ -157,27 +182,120 @@ def parseBrugData(brugSchema:xmlschema.XMLSchema, brugConfig:str):
             rb.addRijstrook(Rijstrook(rb,rijstrook['@breedte'],0,sum(x.width for x in rb.rijstroken)+rb.y,rijstrook['@uuid'],rijstrook['@verkeersSoort']))
     return brug
 
-if __name__ == "__main__":
-    tree = ET.parse('config.xml')
+class IRCamera(Point):
+    def __init__(self, x: float, y: float, cameraId: str, cam: ct.Camera):
+        super().__init__(x, y)
+        self.cam = cam
+        self.cameraId = cameraId
+        self.detectionZones = []
+
+    @property
+    def cam(self):
+        return self._cam
+
+    @cam.setter
+    def cam(self,cam):
+        self._cam = cam
+
+    @property
+    def cameraId(self):
+        return self._cameraId
+
+    @cameraId.setter
+    def cameraId(self,cameraId):
+        self._cameraId = cameraId
+
+    def addDetectionZone(self, detectionZone):
+        if len(detectionZone.points) == 0:
+            detectionZone.points = self.cam.spaceFromImage(np.array(detectionZone.shapePx))
+        self.detectionZones.append(detectionZone)
+
+class IRDetectionZone(Polygon):
+    def __init__(self, shapePx: list, mode: dict, zoneId: str, shape: list = []):
+        super().__init__(shape)
+        self.shapePx = shapePx
+        self.mode = mode
+        self.zoneId = zoneId
+
+    @property
+    def shapePx(self):
+        return self._shapePx
+
+    @shapePx.setter
+    def shapePx(self, shapePx):
+        self._shapePx = shapePx
+
+    @property
+    def mode(self):
+        return self._mode
+
+    @mode.setter
+    def mode(self, mode):
+        self._mode = mode
+
+    @property
+    def zoneId(self):
+        return self._zoneId
+
+    @zoneId.setter
+    def zoneId(self, zoneId):
+        self._zoneId = zoneId
+
+def parseIRData(configFile: str, frameSize: tuple):
+    # Parse xml
+    tree = ET.parse(configFile)
     root = tree.getroot()
-
-    # for child in root:
-    #     print(child.tag, child.attrib)
-
-    brugSchema = xmlschema.XMLSchema('Brug.xsd')
-    brugConfig = 'brugConfig.xml'
-    if brugSchema.is_valid(brugConfig):
-        brug = parseBrugData(brugSchema,brugConfig)
-        # bruigConfigDict = brugSchema.to_dict('brugConfig.xml')
-    # irSchema = xmlschema.XMLSchema('IR.xsd')
-    lidarSchema = xmlschema.XMLSchema('Lidar.xsd')
-    lidarSchema.is_valid('lidarConfig.xml')
-
+    # Find ressrouces
     TiltSensorCalibration = root.find('TiltSensorCalibration')
     Parameters = TiltSensorCalibration.find('Parameters')
     CameraHeight = float(TiltSensorCalibration.get('CameraHeight'))
     TiltAngle = float(Parameters.get('TiltAngle'))
     RollAngle = float(Parameters.get('RollAngle'))
+    PanAngle = float(Parameters.get('PanAngle'))
     CcdWidth = float(Parameters.get('CcdWidth'))
     CcdHeight = float(Parameters.get('CcdHeight'))
     FocalDistance = float(Parameters.get('FocalDistance'))
+    # Create camera object
+    cam = ct.Camera(ct.RectilinearProjection(focallength_mm=FocalDistance,
+                                        sensor_width_mm=CcdWidth*1000,
+                                        sensor_height_mm=CcdHeight*1000,
+                                        image_width_px=frameSize[0],
+                                        image_height_px=frameSize[1],),
+            ct.SpatialOrientation(elevation_m=CameraHeight,
+                                    tilt_deg=90.0-TiltAngle,
+                                    roll_deg=RollAngle,
+                                    heading_deg=PanAngle))
+    # Get unique camera name
+    cameraName = root.find('General').find('CameraName').get('Value')
+    # Create IRCamera object
+    irCamera = IRCamera(0,0,cameraName,cam)
+    # Find detection zones
+    PedestrianPresence = root.find('PedestrianPresence')
+    if PedestrianPresence is not None:
+        Zones = PedestrianPresence.find('PedestrianPresence').find('Zones')
+        for Zone in Zones.iter('Zone'):
+            points = Zone.find('Shape').findall('Point')
+            shape = list(tuple(int(value) for value in point.attrib.values()) for point in points)
+            mode = Zone.find('DetectionMode').attrib
+            # Create IRDetectionZone
+            irDetectionZone = IRDetectionZone(shape, mode, Zone.get('ZoneId'))
+            # Add zone to IRCamera
+            irCamera.addDetectionZone(irDetectionZone)
+
+    return irCamera
+
+
+if __name__ == "__main__":
+    
+
+    brugSchema = xmlschema.XMLSchema('Brug.xsd')
+    brugConfig = 'brugConfig.xml'
+    if brugSchema.is_valid(brugConfig):
+        brug = parseBrugData(brugSchema,brugConfig)
+    
+    # irSchema = xmlschema.XMLSchema('IR.xsd')
+    irCamera = parseIRData('irConfig.xml',(320,240))
+
+    lidarSchema = xmlschema.XMLSchema('Lidar.xsd')
+    lidarSchema.is_valid('lidarConfig.xml')
+
