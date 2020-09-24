9e258a44675106dad8074172435352f8047bbda0
YannRosemaRWS <yann.rosema@rws.nl>
2020-09-24 10:07:49 +0200

    use new websocket_client package

--- main.py
+++ main.py
@@ -1,58 +1,24 @@
-import requests
-import asyncio
-import websockets
+# import requests
+# import asyncio
+import websocket
 import logging
 import json
+
 logger = logging.getLogger('websockets')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 
-async def receiveMessage(closing,closed):
-    uri = "ws://10.0.0.26:13314/api/subscriptions"
-    async with websockets.connect(uri) as websocket:
-        request = json.dumps({"messageType":"Subscription","subscription":{"type":"Event","action":"Subscribe"}})
+def on_message(ws, message):
+    logger.debug(f"Message received: {message}")
+    # print(f"{message}")
 
-        await websocket.send(request)
-
-        response = await websocket.recv()
-        jsonResponse = json.loads(response)
-        if jsonResponse["subscription"]["returnValue"] == "OK":
-            while not closing.is_set():
-                try:
-                    response = await asyncio.wait_for(websocket.recv(), timeout=1.0)
-                    jsonResponse = json.loads(response)
-                    logger.info(f"{jsonResponse}")
-                except asyncio.TimeoutError as e:
-                    logger.debug(f"Timeout!")
-
-    closed.set()
-
-async def main():
-    r = requests.get('http://10.0.0.26:13314/api/device/info')
-    logger.debug(f"{r.text}")
-
-    closing = asyncio.Event()
-    closed = asyncio.Event()
-
-    logger.debug(f"Starting IR receiver task")
-    subscriptionTask = asyncio.create_task(receiveMessage(closing,closed))
-
-    sleepTime = 10.0
-    logger.debug(f"Sleeping for {sleepTime}s, zzz...")
-    await asyncio.sleep(sleepTime)
-
-    logger.debug(f"Closing..")
-    closing.set()
-    await closed.wait()
-
-    logger.debug(f"Everything closed!")
+def on_open(ws):
+    logger.debug(f"Opening websocket")
+    logger.debug(f"Creating request")
+    request = json.dumps({"messageType":"Subscription","subscription":{"type":"Event","action":"Subscribe"}})
+    logger.debug(f"Sending request: {request}")
+    ws.send(request)
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(main())
-    except Exception as e:
-        pass
-    finally:
-        loop.close()
-
+    ws = websocket.WebSocketApp('ws://10.0.0.26:13314/api/subscriptions', on_message = on_message,on_open=on_open)
+    ws.run_forever()
